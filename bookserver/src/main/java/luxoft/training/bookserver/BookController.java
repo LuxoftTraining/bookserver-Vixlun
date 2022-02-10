d82fab494b33b87a895a17f1cc08fd02a7b33d9a
Мельник Леонид Андреевич (4041126) <lmelnik@dev.vtb.ru>
2022-02-10 22:16:35 +0300

    added book service, added index for boo name column

--- bookserver/src/main/java/luxoft/training/bookserver/BookController.java
+++ bookserver/src/main/java/luxoft/training/bookserver/BookController.java
@@ -1,50 +1,36 @@
 package luxoft.training.bookserver;
 
 import luxoft.training.bookserver.model.Book;
-import luxoft.training.bookserver.repositories.BookRepository;
+import luxoft.training.bookserver.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.IntStream.range;
 
 @RestController
 public class BookController {
 
     @Autowired
-    private BookRepository bookRepository;
+    private BookService bookService;
 
     @GetMapping("/books")
     public List<Book> getBooks() {
-        return bookRepository.findAll();
+        return bookService.getAllBooks();
     }
 
     @GetMapping("/similar/{bookName}")
     public List<Book> getBooksWithName(@PathVariable String bookName) {
-        return Arrays.stream(bookName.split("\\s+")).map(str -> bookRepository.findBookByNameContaining(str))
-                .reduce((books, books2) -> {
-                    books.retainAll(books2);
-                    return books;
-                }).orElse(null);
+        return bookService.getBooksWithName(bookName);
     }
 
     @PostMapping("/generate/{count}")
     public void generateBooks(@PathVariable int count) {
-        bookRepository.saveAll(range(0, count)
-                .mapToObj(value -> new Book(UUID.randomUUID().toString())).collect(toList()));
+        bookService.generateBooks(count);
     }
 
     @PostMapping("/addBook")
     public void addBook(@RequestBody Book book) {
-        bookRepository.save(book);
+        bookService.addBook(book);
     }
 
 }
---
+++ bookserver/src/main/java/luxoft/training/bookserver/service/BookService.java
@@ -0,0 +1,42 @@
+package luxoft.training.bookserver.service;
+
+import luxoft.training.bookserver.model.Book;
+import luxoft.training.bookserver.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static java.util.stream.IntStream.range;
+
+@Component
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public List<Book> getBooksWithName(String bookName) {
+        return Arrays.stream(bookName.split("\\s+")).map(str -> bookRepository.findBookByNameContaining(str))
+                .reduce((books, books2) -> {
+                    books.retainAll(books2);
+                    return books;
+                }).orElse(null);
+    }
+
+    @Transactional
+    public void generateBooks(int count) {
+        range(0, count).forEach(value -> addBook(new Book(UUID.randomUUID().toString())));
+    }
+
+    @Transactional
+    public void addBook(Book book) {
+        bookRepository.save(book);
+    }
+
+}
